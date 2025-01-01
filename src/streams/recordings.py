360a82995d3fda517dd7b4bbc94a8fca32a4b1cc
m8u <mzgtootoo@gmail.com>
2025-01-01 16:32:19 +0300

    implement occupation & extension

--- src/streams/recordings.py
+++ src/streams/recordings.py
@@ -2,15 +2,14 @@ import asyncio
 from dataclasses import dataclass
 from datetime import datetime, timedelta
 from src.core.ffmpeg.ffmpeg import RTSP_TIMEOUT_SECONDS, FFmpeg
-from src.streams.repo import StreamsRepo
+from src.streams.repo import Stream, StreamsRepo
 from src.settings import Settings
 from src.streams.utils import get_reconnection_interval
 
 
 @dataclass
 class Recording:
-    stream_url: str
-    output_dir: str
+    stream: Stream
     ffmpeg: FFmpeg
     task: asyncio.Task
     is_healthy: bool = False
@@ -25,16 +24,18 @@ async def manage() -> None:
     recordings: dict[str, Recording] = {}
 
     while True:
+        await asyncio.sleep(1)
+
         # check if any streams were deleted and no longer are subjects to record
         streams = await streams_repo.get_all()
-        recordings_to_stop = {guid for guid in recordings} - {stream["guid"] for stream in streams}
+        recordings_to_stop = {guid for guid in recordings} - {stream.guid for stream in streams}
         for guid in recordings_to_stop:
             recordings[guid].ffmpeg.stop()
             recordings.pop(guid)
 
         # extend occupation
         for guid in recordings:
-            await streams_repo.occupy_for(guid, settings.STREAM_OCCUPATION_TIME)
+            await streams_repo.extend(guid, settings.STREAM_OCCUPATION_TIME)
 
         # check tasks for exceptions
         done, _ = await asyncio.wait((r.task for r in recordings.values()), timeout=0.1)
@@ -56,19 +57,18 @@ async def manage() -> None:
                     recording.is_healthy = True
                     continue
                 recording.task = asyncio.create_task(
-                    recording.ffmpeg.record(recording.stream_url, recording.output_dir, settings.SEGMENT_TIME),
+                    recording.ffmpeg.record(recording.stream.url, recording.stream.name, settings.SEGMENT_TIME),
                     name=guid,
                 )
 
-        # if streams quota is not met, check for unoccupied streams
-        if len(recordings) < settings.STREAMS_PER_REPLICA and (stream := streams_repo.get_one()):
-            # occupy and start recording
-            await streams_repo.occupy_for(stream["guid"], settings.STREAM_OCCUPATION_TIME)
+        # if streams quota isn't met, check for unoccupied streams
+        if len(recordings) < settings.STREAMS_PER_REPLICA:
+            stream = await streams_repo.occupy(settings.STREAM_OCCUPATION_TIME)
+            if stream is None:
+                continue
             ffmpeg = FFmpeg()
             task = asyncio.create_task(
-                ffmpeg.record(stream["url"], stream["name"], settings.SEGMENT_TIME),
-                name=stream["guid"],  # stream guid as task name for later identification
+                ffmpeg.record(stream.url, stream.name, settings.SEGMENT_TIME),
+                name=stream.guid,  # stream guid as task name for later identification
             )
-            recordings[stream["guid"]] = Recording(stream["url"], stream["name"], ffmpeg, task)
-
-        await asyncio.sleep(1)
+            recordings[stream.guid] = Recording(stream.url, stream.name, ffmpeg, task)
--- src/streams/repo.py
+++ src/streams/repo.py
@@ -1,23 +1,70 @@
-from datetime import datetime
+import asyncio
+from dataclasses import dataclass
+import logging
+import random
+from uuid import uuid4
 import json
 import redis.asyncio as redis
 
 from src.core.singleton import Singleton
 from src.settings import Settings
 
+logger = logging.getLogger(__name__)
+
 KEY_PREFIX = "ffnvr"
+
+
+@dataclass
+class Stream:
+    guid: str
+    url: str
+    name: str
 
 
 class StreamsRepo(metaclass=Singleton):
     def __init__(self):
-        settings = Settings()
-        self.redis = redis.from_url(settings.REDIS_DSN)
+        self.settings = Settings()
+        self._redis = redis.from_url(self.settings.REDIS_DSN)
+        self._uuid = uuid4()
 
-    async def add(self, guid: str, name: str, url: str) -> None:
-        await self.redis.set(f"{KEY_PREFIX}-stream-{guid}", json.dumps({"name": name, "url": url}))
+    async def add(self, stream: Stream) -> None:
+        await self._redis.set(
+            f"{KEY_PREFIX}-stream-{stream.guid}",
+            json.dumps({"guid": stream.guid, "name": stream.name, "url": stream.url}),
+        )
 
     async def delete(self, guid: str) -> None:
-        await self.redis.delete(f"{KEY_PREFIX}-stream-{guid}")
+        await self._redis.delete(f"{KEY_PREFIX}-stream-{guid}")
 
-    async def occupy_for(self, guid: str, seconds: int) -> None:
-        await self.redis.set(f"{KEY_PREFIX}-stream-{guid}", datetime.now().isoformat()) # ну там чет там да все я спать
+    async def get_all(self) -> list[Stream]:
+        keys = await self._redis.keys(f"{KEY_PREFIX}-stream-*")
+        streams = []
+        for key in keys:
+            stream_data = json.loads(await self._redis.get(key))
+            streams.append(Stream(guid=stream_data.guid, name=stream_data.name, url=stream_data.url))
+        return streams
+
+    async def occupy(self, seconds: int) -> Stream | None:
+        """
+        Try to occupy an unoccupied stream, and return it.
+        If there are no unoccupied streams, return None.
+        """
+        while True:
+            stream_keys = await self._redis.keys(f"{KEY_PREFIX}-stream-*")
+            lock_keys = await self._redis.keys(f"{KEY_PREFIX}-lock-*")
+            unoccupied = set(k.removeprefix(f"{KEY_PREFIX}-stream-") for k in stream_keys) - set(
+                k.removeprefix(f"{KEY_PREFIX}-lock-") for k in lock_keys
+            )
+            for guid in unoccupied:
+                ok = await self._redis.set(f"{KEY_PREFIX}-lock-{guid}", self._uuid, ex=seconds, nx=True)
+                logger.debug(f"redis returned {ok} when trying to occupy stream")
+                if not ok:
+                    await asyncio.sleep(random.random())
+                    continue
+                stream_data = json.loads(await self._redis.get(f"{KEY_PREFIX}-stream-{guid}"))
+                return Stream(guid=stream_data.guid, name=stream_data.name, url=stream_data.url)
+
+            return None
+
+    async def extend(self, guid: str, seconds: int) -> None:
+        await self._redis.set(f"{KEY_PREFIX}-lock-{guid}", self._uuid, ex=seconds, nx=False)
